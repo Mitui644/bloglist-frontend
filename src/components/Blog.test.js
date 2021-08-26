4f73ddfdf14fceaae19f8eea855f8bc01101c374
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-26 18:26:18 +0300

    Solution for 5_14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,20 +1,49 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
+describe('<Blog />', () => {
+  let component
+
   const blog = {
-    title: 'test_title', author: 'test_author', url: 'test_url', likes: 0
+    author: 'test_author',
+    id: '612643f241c93c5580d4e65f',
+    likes: 0,
+    title: 'test_title',
+    url: 'test_url',
+    user: {
+      username: 'pete',
+      name: 'petes name here',
+      id: '612614ed6121223be561de52'
+    }
   }
 
-  const component = render(
-    <Blog blog={blog} addLike={jest.fn()} deleteBlog={jest.fn()} showDeleteButton={false}/>
-  )
 
-  expect(component.container).toHaveTextContent('test_title')
-  expect(component.container).toHaveTextContent('author')
+  beforeEach(() => {
+    component = render(
+      <Blog blog={blog} addLike={jest.fn()} deleteBlog={jest.fn()} showDeleteButton={false}/>
+    )
+  })
 
-  expect(component.container).not.toHaveTextContent('test_url')
-  expect(component.container).not.toHaveTextContent('likes ' + blog.likes)
+  test('renders content without details at start', () => {
+
+    expect(component.container).toHaveTextContent('test_title')
+    expect(component.container).toHaveTextContent('author')
+
+    expect(component.container).not.toHaveTextContent('test_url')
+    expect(component.container).not.toHaveTextContent('likes ' + blog.likes)
+  })
+
+  test('after clicking the button view, details are displayed', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent('test_title')
+    expect(component.container).toHaveTextContent('author')
+
+    expect(component.container).toHaveTextContent('test_url')
+    expect(component.container).toHaveTextContent('likes ' + blog.likes)
+  })
+
 })
