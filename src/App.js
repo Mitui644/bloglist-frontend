a66f169dfbafb1d459928e01405cfc893ecf8558
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-26 15:16:47 +0300

    Solution for 5_9

--- src/App.js
+++ src/App.js
@@ -173,7 +173,7 @@ const App = () => {
         <Togglable buttonLabel='create new blog' ref={blogFormToggleRef}>
           <BlogForm addBlog={addBlog}/>
         </Togglable>
-        {blogs.map(blog =>
+        {[...blogs].sort((a, b) => b.likes - a.likes).map(blog =>
           <Blog key={blog.id} blog={blog} addLike={addLike} />
         )}
         </>
