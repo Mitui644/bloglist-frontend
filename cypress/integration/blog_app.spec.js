3d64131b3353586707f971cf8e30fb2ea8439d91
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-27 13:20:22 +0300

    Solution for 5_18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'root',
+      username: 'exampleUsername',
+      password: 'examplePassword'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user) 
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +14,28 @@ describe('Blog app', function() {
     cy.contains('log in to application')
     cy.contains('username').find('input')
     cy.contains('password').find('input')
+  })
+
+  describe('Login',function() {
+
+    it('succeeds with correct credentials', function() {
+      cy.get('input:first').type('exampleUsername')
+      cy.get('input:last').type('examplePassword')
+      cy.contains('login').click()
+      cy.contains('exampleUsername logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input:first').type('not real username')
+      cy.get('input:last').type('not real password')
+      cy.contains('login').click()
+
+      cy.get('.error')
+      .should('contain', 'wrong username or password')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'exampleUsername logged in')
+    })
   })
 })
